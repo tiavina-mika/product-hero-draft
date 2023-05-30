242cb95f85bc6f0e9b981f888a0e61004e74803c
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-05-30 21:35:53 +0300

    add form

---
+++ src/components/form/Form.tsx
@@ -0,0 +1,35 @@
+import { Stack, Button, SxProps, Theme, Alert } from '@mui/material';
+import { FormEvent, ReactNode } from 'react';
+import { FormProvider } from 'react-hook-form';
+
+type Props = {
+  onSubmit?: (() => void) | ((event: FormEvent<HTMLFormElement>) => void);
+  form?: any;
+  loading?: boolean;
+  children?: ReactNode;
+  primaryButtonText?: string;
+  error?: string;
+  buttonSx?: SxProps<Theme>;
+};
+
+const Form = ({ onSubmit, form, error, children, primaryButtonText, loading, buttonSx }: Props) => {
+  return (
+    <FormProvider {...form}>
+      <form onSubmit={onSubmit} className="stretchSelf">
+        {error && (
+          <Alert severity="error" sx={{ mb: 1.5 }}>
+            {error}
+          </Alert>
+        )}
+        <Stack spacing={3} justifyContent="center">
+          {children}
+          <Button type="submit" variant="contained" className="endSelf" sx={buttonSx}>
+            {loading ? '...' : primaryButtonText}
+          </Button>
+        </Stack>
+      </form>
+    </FormProvider>
+  );
+};
+
+export default Form;

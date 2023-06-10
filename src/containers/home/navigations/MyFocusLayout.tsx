0bc8d9c908061e7aff7efd5c2b8b2cc526c3235b
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-06-10 11:13:14 +0300

    update top

--- src/containers/home/navigations/MyFocusLayout.tsx
+++ src/containers/home/navigations/MyFocusLayout.tsx
@@ -2,9 +2,8 @@
 /* @jsx jsx */
 /** @jsxImportSource @emotion/react */
 import { jsx } from "@emotion/react";
-import { Box, Stack, Typography } from "@mui/material";
+import { IconButton, Stack, Typography } from "@mui/material";
 import { ReactNode } from "react";
-import PageLayout from "../../../components/layouts/PageLayout";
 import UserAvatar from "../../../components/UserAvatar";
 import { users } from "../../../utils/data/user";
 import { getUserFullName } from "../../../utils/user.utils";
@@ -13,26 +12,51 @@ const currentUser = users[0];
 const workspace = "Roudoudou Inc.";
 const role = "Administrateur";
 
+const classes = {
+  top: {
+    padding: 24
+  },
+  content: {
+    padding: 16
+  }
+};
 type Props = {
   children: ReactNode;
 };
 const MyFocusLayout = ({ children }: Props) => {
   return (
-    <PageLayout className="flexColumn flex1 stretchSelf">
+    <div className="flexColumn flex1 stretchSelf">
       {/* --------- header ------- */}
-      <Stack direction="row" className="center" spacing={1.3}>
-        <UserAvatar user={currentUser} size={46} />
-        <Stack spacing={0.5} className="justifyCenter">
-          <Typography variant="h6">{getUserFullName(currentUser)}</Typography>
-          <Typography variant="h3">{workspace}</Typography>
-          <Typography variant="body2">{role}</Typography>
+      <div css={classes.top} className="flexRow spaceBetween stretchSelf">
+        {/* left */}
+        <Stack direction="row" className="center" spacing={1.7}>
+          <UserAvatar user={currentUser} size={46} />
+          <Stack spacing={0.58} className="justifyCenter">
+            <Typography variant="h6">{getUserFullName(currentUser)}</Typography>
+            <Typography variant="h3">{workspace}</Typography>
+            <Typography variant="body2">{role}</Typography>
+          </Stack>
+          <div className="stretchSelf flexCenter">
+            <img alt="chevron-down" src="/icons/chevron-down.svg" />
+          </div>
         </Stack>
-      </Stack>
-      <Box sx={{ mt: 5 }}>
+        {/* right */}
+        <div>
+          <Stack direction="row" className="center" spacing={1}>
+            <IconButton>
+              <img alt="search" src="/icons/search.svg" />
+            </IconButton>
+            <IconButton>
+              <img alt="setting" src="/icons/app-setting.svg" />
+            </IconButton>
+          </Stack>
+        </div>
+      </div>
+      <div css={classes.content}>
         {/* simulate React Router outlet component */}
         {children}
-      </Box>
-    </PageLayout>
+      </div>
+    </div>
   );
 };
 
